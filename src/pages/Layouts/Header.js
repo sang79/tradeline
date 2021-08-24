edd080ad5d01a8d32aa69c3082d0e2fd3d0f74c4
sangnc <ncsang.egamehue@gmail.com>
2021-08-24 09:59:13 +0700

    update interface

--- src/pages/Layouts/Header.js
+++ src/pages/Layouts/Header.js
@@ -4,20 +4,22 @@ export const Header = () => (
     <>
         <div class="bar-1">
             <div class="container">
-                <p className= " title-4 blue blue-100 text-capitalize">hello, world</p>
-                <p className= "title-0 ">1</p>
-                <p className= "title-1">2</p>
-                <p className= "title-2">3</p>
-                <p className= "title-3">4</p>
-                <p className= "title-4">5</p>
-                <p className= "title-5">5</p>
-                <p className= "heading-1  ">1</p>
-                <p className= "heading-2">2</p>
-                <p className= "subtitle">3</p>
-                <p className= "paragraph-1">4</p>
-                <p className= "paragraph-2">5</p>
-                <p className= "callout">5</p>
-                <div class="col-lg-6"></div>
+                <p className=" title-4 blue blue-100 text-capitalize">hello, world</p>
+                {/* <p className="title-0 ">1</p>
+                <p className="title-1">2</p>
+                <p className="title-2">3</p>
+                <p className="title-3">4</p>
+                <p className="title-4">5</p>
+                <p className="title-5">5</p>
+                <p className="heading-1  ">1</p>
+                <p className="heading-2">2</p>
+                <p className="subtitle">3</p>
+                <p className="paragraph-1">4</p>
+                <p className="paragraph-2">5</p>
+                <p className="callout1">5</p>
+                <p className="callout">6</p>
+                <p className="footnote">7</p>
+                <div class="col-lg-6"></div> */}
             </div>
             <nav class="navbar navbar-expand-lg navbar-dark ">
                 <div class="container-fluid">
@@ -52,6 +54,13 @@ export const Header = () => (
                 </div>
             </nav>
         </div>
-        <button className = "search-btn-lg bg-secondary text-white">hi</button>
+        <div className="mt-3">
+            <button className="search-btn-lg bg-primary text-white"></button>
+            <button className=" btn-sm star text-secondary fw-bold"></button>
+            <button className="btn-lg bg-primary text-white mx-2"></button>
+            <button className="search-btn-sm bg-primary text-white"></button>
+            <button className="btn-sm bg-primary text-white">button</button>
+            <button className="search-btn-lg--specify bg-primary text-white">btn</button>
+        </div>
     </>
 )
